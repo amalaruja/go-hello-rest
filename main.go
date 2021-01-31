611ea11bb9eba086e16cc37203c456da8bbe79ce
amalaruja <amalaruja@gmail.com>
2021-01-31 09:12:58 +0530

    Adds command line arg to modify service response

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"net/http"
-	
+	"flag"
+
 	"github.com/amalaruja/go-hello-rest/service"
 )
 
 func main() {
-	helloService := service.Hello{Message: "DC 1"}
+	namePtr := flag.String("name", "DC 1", "Name")
+	flag.Parse()
+
+	helloService := service.Hello{Message: *namePtr}
+
     http.HandleFunc("/hello", helloService.SayHello)
     http.ListenAndServe(":8080", nil)
 }
